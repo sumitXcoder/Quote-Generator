376b0309121742bdf94036fb9ad94c8ec5597189
Sumit Kumar <118156583+sumitXcoder@users.noreply.github.com>
2023-04-02 11:32:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   const [loading, setLoading] = useState(false)
   function newQuote() {
     fetch('https://api.api-ninjas.com/v1/quotes?category=' + title[index],
-      { headers: { 'X-Api-Key': 'rUCUlQUkH4TdMYrRP98CRA==KWcuwqwmxJ3aQAzR' } })
+      { headers: { 'X-Api-Key': MY_API_KEY } })
       .then(response => response.json())
       .then(data => {
         setQuote(data[0]);
